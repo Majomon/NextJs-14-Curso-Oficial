871e66f83d82174bf1f6fd004d5cc5be19b71e58
Mauri <mauri.monzon91@gmail.com>
2023-11-24 13:30:59 -0300

    Fetch con suspende y fallback

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,10 @@ import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import { lusitana } from '../ui/fonts';
+import { Suspense } from 'react';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
 
 export default async function Dashboard() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -22,8 +22,12 @@ export default async function Dashboard() {
       /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices/>
+        </Suspense>
       </div>
     </main>
   );
